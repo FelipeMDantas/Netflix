14a00f7ed7f581d96b58d23d1123bda891f22ab3
FelipeMDantas <felipemdantas@terra.com.br>
2022-07-31 23:27:15 -0300

    Video and Netflix pages customization.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Netflix from "./pages/Netflix";
+import Player from "./pages/Player";
 import SignUp from "./pages/SignUp";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="player" element={<Player />} />
         <Route exact path="/" element={<Netflix />} />
       </Routes>
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { FaPowerOff, FaSearch } from "react-icons/fa";
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 import logo from "../assets/logo.png";
 import { firebaseAuth } from "../utils/firebase-config";
@@ -17,6 +17,13 @@ const Navbar = ({ isScrolled }) => {
     { name: "Movies", link: "/movies" },
     { name: "My List", link: "/mylist" },
   ];
+
+  const navigate = useNavigate();
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (!currentUser) navigate("/login");
+  });
+
   return (
     <div>
       <Container>
@@ -26,7 +33,7 @@ const Navbar = ({ isScrolled }) => {
               <img src={logo} alt="logo" />
             </div>
             <ul className="links flex">
-              {links.map((name, link) => {
+              {links.map(({ name, link }) => {
                 return (
                   <li key={name}>
                     <Link to={link}>{name}</Link>
@@ -64,6 +71,106 @@ const Navbar = ({ isScrolled }) => {
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  .scrolled {
+    background-color: black;
+  }
+
+  nav {
+    top: 0;
+    height: 6.5rem;
+    width: 100%;
+    justify-content: space-between;
+    position: fixed;
+    z-index: 2;
+    padding: 0 4rem;
+    align-items: center;
+    transition: 0.3s ease-in-out;
+
+    .left {
+      gap: 2rem;
+
+      .brand {
+        img {
+          height: 4rem;
+        }
+      }
+
+      .links {
+        list-style-type: none;
+        gap: 2rem;
+
+        li {
+          a {
+            color: white;
+            text-decoration: none;
+          }
+        }
+      }
+    }
+
+    .right {
+      gap: 1rem;
+
+      button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        &:focus {
+          outline: none;
+        }
+
+        svg {
+          color: #f34242;
+          font-size: 1.2rem;
+        }
+      }
+
+      .search {
+        display: flex;
+        gap: 0.4rem;
+        align-items: center;
+        justify-content: center;
+        padding: 0.2rem;
+        padding-left: 0.5rem;
+
+        button {
+          background-color: transparent;
+
+          svg {
+            color: white;
+          }
+        }
+
+        input {
+          width: 0;
+          opacity: 0;
+          visibility: hidden;
+          transition: 0.3s ease-in-out;
+          background-color: transparent;
+          border: none;
+          background-color: transparent;
+          border: none;
+          color: white;
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+
+      .show-search {
+        border: 1px solid white;
+        background-color: (0, 0, 0, 0.6);
+
+        input {
+          width: 100%;
+          opacity: 1;
+          visibility: visible;
+          padding: 0.3rem;
+        }
+      }
+    }
+  }
+`;
 
 export default Navbar;
